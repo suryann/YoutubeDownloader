0ea06199f491e911049e7a8cc42530b344d0ec9a
Vivek R K <vravindranath@gmail.com>
2012-05-20 14:40:14 +0530

    Changed design: Moved Download code to separate class.

--- src/com/vravindranath/youtubedownloader/VideoDownloader.java
+++ src/com/vravindranath/youtubedownloader/VideoDownloader.java
@@ -1,5 +1,64 @@
 package com.vravindranath.youtubedownloader;
 
-public class VideoDownloader {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class VideoDownloader {
+	private static final String YOUTUBE_WATCH_URL_PREFIX = "http://www.youtube.com/watch?v=";
+	
+	public VideoDownloader(String url) {
+		try {
+			String htmlContent = getHTMLString(url);
+			String downloadUrl = getDownloadUrlFromHtmlContent(htmlContent);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private String getDownloadUrlFromHtmlContent(String htmlContent) {
+		String downloadUrl = null;
+		String[] videoIdMatches = htmlContent.split(/\"video_id\":\s*\"([^\"]+)\"/);
+		return downloadUrl;
+	}
+	
+	private String getHTMLString(String url) throws ClientProtocolException, IOException {
+		url = "http://www.youtube.com/watch?v=1DVusKFmjj4&feature=g-vrec";
+		String html = "";
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+		HttpResponse response = httpClient.execute(request);
+		InputStream stream = response.getEntity().getContent();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		StringBuilder str = new StringBuilder();
+		String line = null;
+		while((line = reader.readLine()) != null) {
+		    str.append(line);
+		}
+		stream.close();
+		html = str.toString();
+		return html;
+	}
+	
+	private String getVideoIdFromUrl(String url) {
+		url = "http://www.youtube.com/watch?v=1DVusKFmjj4&feature=g-vrec";
+		String videoId = null;
+		int indexOfAnd;
+		videoId = url.substring(YOUTUBE_WATCH_URL_PREFIX.length());
+		indexOfAnd = videoId.indexOf('&');
+		videoId = videoId.substring(0, indexOfAnd);
+		return videoId;
+	}
 }
--- src/com/vravindranath/youtubedownloader/YoutubeDownloaderActivity.java
+++ src/com/vravindranath/youtubedownloader/YoutubeDownloaderActivity.java
@@ -1,28 +1,14 @@
 package com.vravindranath.youtubedownloader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class YoutubeDownloaderActivity extends Activity {
-    /** Called when the activity is first created. */
-	private static final String tag = "YoutubeDownloaderActivity";
-	private static final String YOUTUBE_WATCH_URL_PREFIX = "http://www.youtube.com/watch?v=";
+    /** Called when the activity is first created. */	
 	
 	private EditText sourceUrlEditText = null;
     @Override
@@ -40,9 +26,9 @@ public class YoutubeDownloaderActivity extends Activity {
 				String videoUrl = (sourceUrlEditText.getText()).toString();
 //				Log.i(tag, videoUrl);
 				//For testing
-				saveVideoToDevice(videoUrl);
+				VideoDownloader downloader = new VideoDownloader(videoUrl);
 				if(isValidUrl(videoUrl)) {
-					saveVideoToDevice(videoUrl);
+//					VideoDownloader downloader = new VideoDownloader(videoUrl);
 				} else {
 					//Display Error dialog
 				}
@@ -58,49 +44,5 @@ public class YoutubeDownloaderActivity extends Activity {
 	protected boolean isValidUrl(String url) {
 		// TODO: Check if url is valid
 		return false;
-	}
-	
-	private String getHTMLString(String url) throws ClientProtocolException, IOException {
-		url = "http://www.youtube.com/watch?v=1DVusKFmjj4&feature=g-vrec";
-		String html = "";
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-		HttpResponse response = httpClient.execute(request);
-		InputStream stream = response.getEntity().getContent();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-		StringBuilder str = new StringBuilder();
-		String line = null;
-		while((line = reader.readLine()) != null) {
-		    str.append(line);
-		}
-		stream.close();
-		html = str.toString();
-		return html;
-	}
-
-	private void saveVideoToDevice(String url) {
-		//TODO: Add saving logic here
-		try {
-			String html = getHTMLString(url);
-			Log.i(tag, html);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String videoId = getVideoIdFromUrl(url);
-		Log.i(tag, videoId);
-	}
-	
-	private String getVideoIdFromUrl(String url) {
-		url = "http://www.youtube.com/watch?v=1DVusKFmjj4&feature=g-vrec";
-		String videoId = null;
-		int indexOfAnd;
-		videoId = url.substring(YOUTUBE_WATCH_URL_PREFIX.length());
-		indexOfAnd = videoId.indexOf('&');
-		videoId = videoId.substring(0, indexOfAnd);
-		return videoId;
 	}
 }
